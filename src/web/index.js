3b654ef4dd1f1b930da4c41f7be43dcb896528b4
Seyed Ali Ahmadnejad <x01saa@gmail.com>
2020-03-16 11:36:27 +0330

    add craco and semantic theme files

--- src/web/index.js
+++ src/web/index.js
@@ -14,6 +14,7 @@ import Loading from './components/UI/Loading';
 
 // Load css
 import './styles/style.scss';
+import 'semantic-ui-less/semantic.less'
 
 const { persistor, store, dispatch } = configureStore();
 // persistor.purge(); // Debug to clear persist
